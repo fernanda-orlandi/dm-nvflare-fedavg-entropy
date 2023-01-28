Fix CNN for mnist dataset and fix entropy computation

mnist_fedavg_entropy/custom/pt/learners/mnist_learner.py
@@ -23,6 +23,7 @@ from pt.networks.mnist_nets import ModerateCNN
 from pt.utils.mnist_dataset import MNIST_Idx
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import datasets, transforms
+from torchsummary import summary
 
 from nvflare.apis.dxo import DXO, DataKind, MetaKey, from_shareable
 from nvflare.apis.fl_constant import FLContextKey, ReturnCode
@@ -119,7 +120,9 @@ class MNISTLearner(Learner):  # also supports MNISTScaffoldLearner
         # For Entropy analysis
         entropy_file_name = os.path.join(self.dataset_root, "meanentropy.txt")
         with open(entropy_file_name) as file:
-            self.mean_entropy = math.ceil(float(file.readline()))
+            self.mean_entropy = float(file.readline()) # math.ceil(float(file.readline()))
+
+        print(self.mean_entropy)
 
 #        site_entropy = self.entropy(site_idx)
 #        if (site_entropy > self.mean_entropy):
@@ -130,7 +133,10 @@ class MNISTLearner(Learner):  # also supports MNISTScaffoldLearner
         # can be replaced by a config-style block
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.model = ModerateCNN().to(self.device)
+        summary(self.model, (1,28,28))
         self.optimizer = optim.SGD(self.model.parameters(), lr=self.lr, momentum=0.9)
+#        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)
+#        self.optimizer = optim.RMSprop(self.model.parameters(), lr=0.001, alpha=0.9, eps=1e-08, weight_decay=0, momentum=0.9, centered=False, foreach=None)
         self.criterion = torch.nn.CrossEntropyLoss()
         if self.fedproxloss_mu > 0:
             self.log_info(fl_ctx, f"using FedProx loss with mu {self.fedproxloss_mu}")
@@ -138,18 +144,18 @@ class MNISTLearner(Learner):  # also supports MNISTScaffoldLearner
 
         self.transform_train = transforms.Compose(
             [
-                #transforms.ToTensor(),
                 transforms.ToPILImage(),
-                #transforms.Pad(4, padding_mode="reflect"),
-                #transforms.RandomCrop(32),
-                #transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
-                transforms.Normalize((0.1307,), (0.3081,))
+
+                # global Mean and Standard Deviation of the MNIST dataset
+                transforms.Normalize((0.1307,), (0.3081,)) 
             ]
         )
         self.transform_valid = transforms.Compose(
             [
                 transforms.ToTensor(),
+
+                # global Mean and Standard Deviation of the MNIST dataset
                 transforms.Normalize((0.1307,), (0.3081,))
             ]
         )
@@ -172,7 +178,7 @@ class MNISTLearner(Learner):  # also supports MNISTScaffoldLearner
 
         self.train_loader = torch.utils.data.DataLoader(self.train_dataset, batch_size=100, shuffle=True, num_workers=2)
 
-        self.valid_loader = torch.utils.data.DataLoader(self.valid_dataset, batch_size=100, shuffle=True, num_workers=2)
+        self.valid_loader = torch.utils.data.DataLoader(self.valid_dataset, batch_size=100, shuffle=False, num_workers=2)
 
     def finalize(self, fl_ctx: FLContext):
         # collect threads, close files here
@@ -188,9 +194,9 @@ class MNISTLearner(Learner):  # also supports MNISTScaffoldLearner
             self.log_info(fl_ctx, f"Local epoch {self.client_id}: {epoch + 1}/{self.aggregation_epochs} (lr={self.lr})")
             for i, (inputs, labels) in enumerate(train_loader):
 
-                #print(labels)
                 site_entropy = self.entropy(labels)
-                #print(site_entropy)
+#                print(site_entropy)
+
                 if (site_entropy < self.mean_entropy):
 
                     if abort_signal.triggered:
@@ -333,6 +339,7 @@ class MNISTLearner(Learner):  # also supports MNISTScaffoldLearner
         with torch.no_grad():
             correct, total = 0, 0
             for i, (inputs, labels) in enumerate(valid_loader):
+
                 if abort_signal.triggered:
                     return None
                 inputs, labels = inputs.to(self.device), labels.to(self.device)
@@ -341,6 +348,7 @@ class MNISTLearner(Learner):  # also supports MNISTScaffoldLearner
 
                 total += inputs.data.size()[0]
                 correct += (pred_label == labels.data).sum().item()
+
             metric = correct / float(total)
             if tb_id:
                 self.writer.add_scalar(tb_id, metric, self.epoch_global)

mnist_fedavg_entropy/custom/pt/networks/mnist_nets.py
@@ -66,49 +66,34 @@ class ModerateCNN(nn.Module):
         super(ModerateCNN, self).__init__()
         self.conv_layer = nn.Sequential(
             # Conv Layer block 1
-            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
+            nn.BatchNorm2d(8),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Dropout2d(p=0.05),
+
             # Conv Layer block 2
-            #nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
-            #nn.ReLU(inplace=True),
-            #nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
-            #nn.ReLU(inplace=True),
-            #nn.MaxPool2d(kernel_size=2, stride=2),
-            #nn.Dropout2d(p=0.05),
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.2),
+            nn.BatchNorm2d(16),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+                        
             # Conv Layer block 3
-            #nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
-            #nn.ReLU(inplace=True),
-            #nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
-            #nn.ReLU(inplace=True),
-            #nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(32),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
         )
 
         self.fc_layer = nn.Sequential(
-            #nn.Dropout(p=0.1),
-            #nn.Linear(4096, 1024),
-            #nn.ReLU(inplace=True),
-            #nn.Linear(576, 256),
-            #nn.ReLU(inplace=True),
-            # nn.Linear(1024, 512),
-            #nn.Linear(256, 256),
-            #nn.ReLU(inplace=True),
-            #nn.Dropout(p=0.1),
 
-            #nn.Linear(1568, 10),
-
-
-            nn.Linear(1568, 512),
+            nn.Flatten(),
+            nn.Linear(288, 100),
             nn.ReLU(inplace=True),
-            nn.Linear(512, 512),
+            nn.Dropout(p=0.3),
+            nn.Linear(100, 10),
             nn.ReLU(inplace=True),
-            nn.Linear(512, 10),
-            #nn.ReLU(inplace=True),
-            #nn.Dropout(p=0.1),
         )
 
     def forward(self, x):

mnist_fedavg_entropy/custom/pt/utils/prepare_data.py
@@ -139,16 +139,20 @@ def main():
         sum_file.write("Class counts for each client: \n")
         sum_file.write(json.dumps(class_sum))
 
+
+    train_targets = load_mnist_data(args.data_dir)
     entropy_array = []
 
     site_file_path = os.path.join(args.data_dir, "site-")
     for site in range(args.num_sites):
         site_file_name = site_file_path + str(site + 1) + ".npy"
-        np.save(site_file_name, np.array(site_idx[site]))
+        site_idx_arr = np.array(site_idx[site])
+        np.save(site_file_name, site_idx_arr)
         
         # calculating site entropy
-        entropy_array.append(entropy(np.array(site_idx[site])))
+        entropy_array.append(entropy(train_targets[site_idx_arr]))
 
+    # Entropy
     mean_entropy = np.mean(entropy_array)
 
     entropy_file_name = os.path.join(args.data_dir, "meanentropy.txt")